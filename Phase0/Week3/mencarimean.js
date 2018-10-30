ef3d83466784e5c5478b110830a7554db1577548
wildanwr <wildanwr@gmail.com>
2018-10-30 14:33:34 +0700

    updt2

--- Phase0/Week3/mencarimean.js
+++ Phase0/Week3/mencarimean.js
@@ -1,10 +1,11 @@
 function cariMean(arr) {
-  var hasil;
-  for (var i = 0; i <= arr.length - 1; i++) {
-    hasil = hasil + arr[i]
-    console.log(hasil);
+  var hasil = 0;
+  var pembagi = arr.length;
+  for (var i = 0; i < arr.length; i++) {
+    hasil = hasil + arr[i];
   }
-  // return hasil / arr.length
+  var mean = Math.round(hasil / pembagi);
+  return mean;
 }
 
 // TEST CASES
@@ -13,5 +14,3 @@ console.log(cariMean([3, 5, 7, 5, 3])); // 5
 console.log(cariMean([6, 5, 4, 7, 3])); // 5
 console.log(cariMean([1, 3, 3])); // 2
 console.log(cariMean([7, 7, 7, 7, 7])); // 7
-
-// BELUM SELESAI
